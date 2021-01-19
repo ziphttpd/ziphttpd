80a3d8742bb875e5553db48f02592616d01613df
XORveRCOM <contact@xorver.com>
2021-01-19 21:32:57 +0900

    setup.sh 実装

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SCRIPT=$0
+TARGET_DIR=$1
+
+if [ -d "${TARGET_DIR}" ]; then
+	echo "Usage: ${SCRIPT} {install-directory}"
+	exit 1
+fi
+
+SCRIPT_DIR=$(cd $(dirname ${SCRIPT}); pwd)
+cd ${SCRIPT_DIR}
+go build -o ${TARGET_DIR}/ziphttpd cmd/main.go
+
+exit 0
